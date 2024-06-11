8415ece2038f5fb1404f12dee8f578237babe153
Ivan Pachev <mr.pachev@abv.bg>
2024-06-11 17:48:09 +0300

    Add UserProfileController.

--- src/main/java/bg/softuni/pathfinder/web/UserRegisterController.java
+++ src/main/java/bg/softuni/pathfinder/web/UserRegisterController.java
@@ -1,11 +1,10 @@
 package bg.softuni.pathfinder.web;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class RegisterUserController {
+public class UserRegisterController {
 
     @GetMapping("/users/register")
     public String viewUserRegister(){
