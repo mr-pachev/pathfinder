588bfa42edbdcd4db59f63a664266b1dc102604f
Ivan Pachev <mr.pachev@abv.bg>
2024-11-01 09:54:09 +0200

    Improved RegisterDTO.

---
+++ src/main/java/bg/softuni/pathfinder/model/dto/RegisterDTO.java
@@ -0,0 +1,73 @@
+package bg.softuni.pathfinder.model.dto;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.*;
+
+public class RegisterDTO {
+    @NotBlank
+    @Size(min = 3, max = 10)
+    private String username;
+    @NotBlank
+    @Size(min = 3)
+    private String fullName;
+    @Email
+    @NotBlank
+    private String email;
+    @NotNull
+    @Min(14)
+    private Integer age;
+    @NotBlank
+    @Size(min = 4, max = 10)
+    private String password;
+    @NotBlank
+    @Size(min = 4, max = 10)
+    private String confirmPassword;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+}
