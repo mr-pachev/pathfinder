a3c33a2b5850b9a4b6082b340325e14c56298615
Ivan Pachev <mr.pachev@abv.bg>
2024-11-02 19:45:31 +0200

    Added UserRepository and isExistUser in UserService.

--- src/main/java/bg/softuni/pathfinder/service/impl/UserServiceImpl.java
+++ src/main/java/bg/softuni/pathfinder/service/impl/UserServiceImpl.java
@@ -1,13 +1,23 @@
 package bg.softuni.pathfinder.service.impl;
 
+import bg.softuni.pathfinder.repository.UserRepository;
 import bg.softuni.pathfinder.service.UserService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
 
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    //check is exist user by username
     @Override
     public boolean isExistUser(String username) {
-        return false;
+
+       return userRepository.findByUsername(username).isPresent();
     }
 }
