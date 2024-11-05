3b2cfdf2d04b5b549ddc7fcc5c6d4e5e819f0a62
Ivan Pachev <mr.pachev@abv.bg>
2024-11-05 13:51:07 +0200

    Added @ModelAttribute("userLoginDTO") in UserLoginController.

--- src/main/java/bg/softuni/pathfinder/web/UserLoginController.java
+++ src/main/java/bg/softuni/pathfinder/web/UserLoginController.java
@@ -4,9 +4,15 @@ import bg.softuni.pathfinder.model.dto.UserLoginDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class UserLoginController {
+    @ModelAttribute("userLoginDTO")
+    public UserLoginDTO userLoginDTO() {
+        return new UserLoginDTO();
+    }
+
     @GetMapping("/users/login")
     public String viewLoginUser(){
 
@@ -16,8 +22,8 @@ public class UserLoginController {
     @GetMapping("users/login-error")
     public String viewLoginError(Model model) {
         model.addAttribute("showErrorMessage", true);
-        model.addAttribute("loginData", new UserLoginDTO());
         return "login";
     }
 
 }
+
