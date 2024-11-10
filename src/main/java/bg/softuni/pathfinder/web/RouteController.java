151b148211787eda4756f928ad01b008088b34fa
Ivan Pachev <mr.pachev@abv.bg>
2024-11-10 18:50:18 +0200

    Added viewAddRoute method in RouteController.

--- src/main/java/bg/softuni/pathfinder/web/RouteController.java
+++ src/main/java/bg/softuni/pathfinder/web/RouteController.java
@@ -1,6 +1,9 @@
 package bg.softuni.pathfinder.web;
 
+import bg.softuni.pathfinder.model.dto.AddRouteDTO;
 import bg.softuni.pathfinder.model.entity.Route;
+import bg.softuni.pathfinder.model.enums.CategoriesRoute;
+import bg.softuni.pathfinder.model.enums.Level;
 import bg.softuni.pathfinder.service.RouteService;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
@@ -31,65 +34,59 @@ public class RouteController {
     }
 
     //add route
-    @GetMapping("/add-route")
+    @GetMapping("/users/add-route")
     public String viewAddRoute(Model model) {
 
         if (!model.containsAttribute("addRouteDTO")) {
-            model.addAttribute("addRouteDTO", new addRouteDTO());
+            model.addAttribute("addRouteDTO", new AddRouteDTO());
         }
 
-        model.addAttribute("engineType", Engine.values());
-        model.addAttribute("transmissionType", Transmission.values());
-        model.addAttribute("brands", brandService.allBrands());
-        model.addAttribute("models", modelService.allModel());
+        model.addAttribute("level", Level.values());
+        model.addAttribute("category", CategoriesRoute.values());
 
-        if (!model.containsAttribute("isNoMatchModel")) {
-            model.addAttribute("isNoMatchModel", false);
-        }
-
-        return "offer-add";
+        return "add-route";
     }
 
-    @PostMapping("/offers/add")
-    public String creatOffer(@Valid AddOfferDTO addOfferDTO,
-                             BindingResult bindingResult,
-                             RedirectAttributes rAtt) {
-
-        boolean isNoMatchModel = false;
-
-        if (!addOfferDTO.getBrand().isEmpty()) {
-            isNoMatchModel = !brandService.modelsByBrand(addOfferDTO.getBrand())
-                    .contains(addOfferDTO.getModel());
-        }
-
-        if (bindingResult.hasErrors() || isNoMatchModel) {
-            rAtt.addFlashAttribute("addOfferDTO", addOfferDTO);
-            rAtt.addFlashAttribute("org.springframework.validation.BindingResult.addOfferDTO", bindingResult);
-            rAtt.addFlashAttribute("engineType", Engine.values());
-            rAtt.addFlashAttribute("transmissionType", Transmission.values());
-            rAtt.addFlashAttribute("brands", brandService.allBrands());
-            rAtt.addFlashAttribute("models", modelService.allModel());
-
-            if (isNoMatchModel) {
-                rAtt.addFlashAttribute("isNoMatchModel", true);
-            }
-
-            return "redirect:/offers/add";
-        }
-
-        long userId = userHelperService.getUser().getId();
-        long offerId = offerService.addOffer(addOfferDTO, userId);
-
-        return "redirect:/details/" + offerId;
-    }
-
-
-    @GetMapping("/details/{id}")
-    public String viewOfferDetail(@PathVariable("id") long id, Model model) {
-        OfferDTO offerDTO = offerService.getOfferDTOById(id);
-
-        model.addAttribute("offerDTO", offerDTO);
-
-        return "details";
-    }
+//    @PostMapping("/offers/add")
+//    public String creatOffer(@Valid AddOfferDTO addOfferDTO,
+//                             BindingResult bindingResult,
+//                             RedirectAttributes rAtt) {
+//
+//        boolean isNoMatchModel = false;
+//
+//        if (!addOfferDTO.getBrand().isEmpty()) {
+//            isNoMatchModel = !brandService.modelsByBrand(addOfferDTO.getBrand())
+//                    .contains(addOfferDTO.getModel());
+//        }
+//
+//        if (bindingResult.hasErrors() || isNoMatchModel) {
+//            rAtt.addFlashAttribute("addOfferDTO", addOfferDTO);
+//            rAtt.addFlashAttribute("org.springframework.validation.BindingResult.addOfferDTO", bindingResult);
+//            rAtt.addFlashAttribute("engineType", Engine.values());
+//            rAtt.addFlashAttribute("transmissionType", Transmission.values());
+//            rAtt.addFlashAttribute("brands", brandService.allBrands());
+//            rAtt.addFlashAttribute("models", modelService.allModel());
+//
+//            if (isNoMatchModel) {
+//                rAtt.addFlashAttribute("isNoMatchModel", true);
+//            }
+//
+//            return "redirect:/offers/add";
+//        }
+//
+//        long userId = userHelperService.getUser().getId();
+//        long offerId = offerService.addOffer(addOfferDTO, userId);
+//
+//        return "redirect:/details/" + offerId;
+//    }
+//
+//
+//    @GetMapping("/details/{id}")
+//    public String viewOfferDetail(@PathVariable("id") long id, Model model) {
+//        OfferDTO offerDTO = offerService.getOfferDTOById(id);
+//
+//        model.addAttribute("offerDTO", offerDTO);
+//
+//        return "details";
+//    }
 }
